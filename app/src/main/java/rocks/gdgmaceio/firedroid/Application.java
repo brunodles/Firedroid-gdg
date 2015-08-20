6423d915c413b4a64c3c5d612399f0a633064bff
Bruno de Lima <dlimaun@gmail.com>
2015-08-19 22:14:48 -0300

    Create Application class

---
+++ app/src/main/java/rocks/gdgmaceio/firedroid/Application.java
@@ -0,0 +1,10 @@
+package rocks.gdgmaceio.firedroid;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by bruno on 19/08/15.
+ */
+public class Application extends android.app.Application {
+
+}
