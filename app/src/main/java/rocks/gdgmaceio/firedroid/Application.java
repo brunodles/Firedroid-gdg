b44999ba3600e354219805f1470433c2ddaf0c4d
Bruno de Lima <dlimaun@gmail.com>
2015-08-19 22:15:27 -0300

    Override onCreate method

--- app/src/main/java/rocks/gdgmaceio/firedroid/Application.java
+++ app/src/main/java/rocks/gdgmaceio/firedroid/Application.java
@@ -1,10 +1,12 @@
 package rocks.gdgmaceio.firedroid;
 
-import com.firebase.client.Firebase;
-
 /**
  * Created by bruno on 19/08/15.
  */
 public class Application extends android.app.Application {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
