7b4458da9305fdb55c4ca97a4172444f527c72d3
Bruno de Lima <dlimaun@gmail.com>
2015-08-19 23:27:12 -0300

    Add field and button association on java code

--- app/src/main/java/rocks/gdgmaceio/firedroid/LoginActivityFragment.java
+++ app/src/main/java/rocks/gdgmaceio/firedroid/LoginActivityFragment.java
@@ -1,15 +1,21 @@
 package rocks.gdgmaceio.firedroid;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+
 public class LoginActivityFragment extends Fragment {
+
+    private EditText email;
+    private EditText password;
+    private Button signIn;
+    private Button signUp;
+    private Button resetPassword;
 
     public LoginActivityFragment() {
     }
@@ -18,5 +24,16 @@ public class LoginActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        email = (EditText) view.findViewById(R.id.email);
+        password = (EditText) view.findViewById(R.id.password);
+
+        signIn = (Button) view.findViewById(R.id.sign_in);
+        signUp = (Button) view.findViewById(R.id.sign_up);
+        resetPassword = (Button) view.findViewById(R.id.reset_password);
     }
 }
