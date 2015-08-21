b19a848ac120eaad163c20b3c0cb8eeb804f3027
Bruno de Lima <dlimaun@gmail.com>
2015-08-21 00:14:01 -0300

    Add views to fragment

--- app/src/main/java/rocks/gdgmaceio/firedroid/ChatActivityFragment.java
+++ app/src/main/java/rocks/gdgmaceio/firedroid/ChatActivityFragment.java
@@ -1,15 +1,22 @@
 package rocks.gdgmaceio.firedroid;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ChatActivityFragment extends Fragment {
+
+    private EditText message;
+    private TextView messages;
+    private Button send;
 
     public ChatActivityFragment() {
     }
@@ -19,4 +26,21 @@ public class ChatActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_chat, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        message = (EditText) view.findViewById(R.id.message);
+        messages = (TextView) view.findViewById(R.id.messages);
+        send = (Button) view.findViewById(R.id.send);
+
+        send.setOnClickListener(onSendClickListener);
+    }
+
+    private View.OnClickListener onSendClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
 }
